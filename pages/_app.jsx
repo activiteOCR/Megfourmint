b806e8f2143c96562a6b76464a3833bfae02a9cd
activiteOCR <julien.terrier3@wanadoo.fr>
2022-12-04 14:31:42 +0100

    Network changed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,23 +1,29 @@
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 import Head from "next/head";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Goerli;
+const activeChainId = ChainId.Mainnet;
 function MyApp({ Component, pageProps }) {
-    return (
-      <ThirdwebProvider desiredChainId={activeChainId}>
-        <Head>
-          <title>Meg4mint Website</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
-          {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
-          <meta name="description" content="Meg4mint Pixel Art for nostalgics"/>
-          <meta name="keywords" content="Pixel Art, Freemint, No royalties, NFT, NFTs, ETH, NFTartiste, NFTartwork"/>
-        </Head>
-        <Component {...pageProps}/>
-        <Analytics />
-      </ThirdwebProvider>
-    );
-  }
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>Meg4mint Website</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
+        <meta name="description" content="Meg4mint Pixel Art for nostalgics" />
+        <meta
+          name="keywords"
+          content="Pixel Art, Freemint, No royalties, NFT, NFTs, ETH, NFTartiste, NFTartwork"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <Analytics />
+    </ThirdwebProvider>
+  );
+}
 export default MyApp;
