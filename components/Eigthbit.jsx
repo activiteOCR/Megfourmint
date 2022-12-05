48331203f87bdf8a19af1c9a23134b2aef9a5c19
activiteOCR <julien.terrier3@wanadoo.fr>
2022-12-05 20:06:07 +0100

    add iframe

--- components/Eigthbit.jsx
+++ components/Eigthbit.jsx
@@ -1,113 +1,145 @@
-import React from 'react'
-import { useState, useEffect, useContext, createContext } from "react"
+import React from "react";
+import { useState, useEffect, useContext, createContext } from "react";
 import styled from "styled-components";
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import { createTheme } from '@mui/material/styles';
-import { makeStyles } from '@mui/styles';
-import Image from 'next/image';
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { createTheme } from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
+import Image from "next/image";
 
 let theme = createTheme({
   palette: {
     primary: {
-      main: '#07001F',
+      main: "#07001F",
     },
   },
 });
 
 const useStyles = makeStyles({
   titre: {
-    [theme.breakpoints.down('lg')]: {
+    [theme.breakpoints.down("lg")]: {
       marginRight: 100,
     },
   },
 });
 
 const useWindowSize = () => {
-const isSSR = typeof window === "undefined";
-const [windowSize, setWindowSize] = useState({
+  const isSSR = typeof window === "undefined";
+  const [windowSize, setWindowSize] = useState({
     width: isSSR ? 1200 : window.innerWidth,
     height: isSSR ? 800 : window.innerHeight,
-});
+  });
 
-function changeWindowSize() {
-  if(!isSSR){
+  function changeWindowSize() {
+    if (!isSSR) {
       setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    }
   }
-}
 
-React.useEffect(() => {
+  React.useEffect(() => {
     window.addEventListener("resize", changeWindowSize);
 
     return () => {
-        window.removeEventListener("resize", changeWindowSize);
+      window.removeEventListener("resize", changeWindowSize);
     };
-}, []);
+  }, []);
 
-return windowSize;
-}
+  return windowSize;
+};
 
-const MobileComponent = ({classes}) => 
+const MobileComponent = ({ classes }) => (
   <ContentMob>
     <div>
-      <h1><u><span className="h1color">8</span>-<span className="h1color">BIT</span></u></h1>
-      <h2><br></br> A robot may not harm a human being or, remaining passive, allow a human being to be exposed to danger.<br></br><br></br>
-      2. A robot must obey the orders given to it by a human being, unless such orders conflict with the First Law.<br></br><br></br>
-      3. A robot must protect its existence so long as such protection does not conflict with the First or Second Law.<br></br><br></br>
-      4. Any robot has the right to reproduce itself, as long as this does not contradict the first, second or third law of robotics.<br></br></h2>
+      <h1>
+        <u>
+          <span className="h1color">8</span>-
+          <span className="h1color">BIT</span>
+        </u>
+      </h1>
+      <h2>
+        <br></br> A robot may not harm a human being or, remaining passive,
+        allow a human being to be exposed to danger.<br></br>
+        <br></br>
+        2. A robot must obey the orders given to it by a human being, unless
+        such orders conflict with the First Law.<br></br>
+        <br></br>
+        3. A robot must protect its existence so long as such protection does
+        not conflict with the First or Second Law.<br></br>
+        <br></br>
+        4. Any robot has the right to reproduce itself, as long as this does not
+        contradict the first, second or third law of robotics.<br></br>
+      </h2>
       <h3>IA - JWC - HH</h3>
     </div>
     <Box
+      component="img"
+      sx={{
+        // height: 300,
+        // width: 300,
+        maxHeight: { xs: 200, md: 300 },
+        maxWidth: { xs: 200, md: 300 },
+        marginRight: "auto",
+        marginLeft: "auto",
+        marginTop: "40px",
+        marginBottom: "40px",
+      }}
+      alt="mega"
+      src="/images/mega.gif"
+    />
+  </ContentMob>
+);
+const DesktopComponent = ({ classes }) => (
+  <Box>
+    <Content>
+      <div>
+        <div className="titre">
+          <Image
+            src="/images/Image1.png" // Route of the image file
+            height={252} // Desired size with correct aspect ratio
+            width={789} // Desired size with correct aspect ratio
+            alt="titre"
+          />
+        </div>
+        <h1>
+          <u>
+            <span className="h1color">8</span>-
+            <span className="h1color">BIT</span>
+          </u>
+        </h1>
+        <h2>
+          <br></br> 1. A robot may not harm a human being or, remaining passive,
+          allow a human being to be exposed to danger.<br></br>
+          <br></br>
+          2. A robot must obey the orders given to it by a human being, unless
+          such orders conflict with the First Law.<br></br>
+          <br></br>
+          3. A robot must protect its existence so long as such protection does
+          not conflict with the First or Second Law.<br></br>
+          <br></br>
+          4. Any robot has the right to reproduce itself, as long as this does
+          not contradict the first, second or third law of robotics.<br></br>
+          <br></br>
+        </h2>
+        <h3>IA - JWC - HH</h3>
+      </div>
+      <Box
         component="img"
         sx={{
-          // height: 300,
-          // width: 300,
-          maxHeight: { xs: 200, md: 300 },
-          maxWidth: { xs: 200, md: 300 },
-          marginRight: "auto",
-          marginLeft: "auto",
-          marginTop: "40px",
-          marginBottom: "40px",
+          maxHeight: { md: 300 },
+          maxWidth: { md: 300 },
+          marginTop: "325px",
+          marginRight: "150px",
         }}
         alt="mega"
         src="/images/mega.gif"
       />
-  </ContentMob>
-;
-
-const DesktopComponent = ({classes}) =>
- <Box>
-  <Content>
-      <div>
-        <div className="titre">
-          <Image
-                src="/images/Image1.png" // Route of the image file
-                height={252} // Desired size with correct aspect ratio
-                width={789} // Desired size with correct aspect ratio
-                alt="titre"
-              />
-          </div>
-        <h1><u><span className="h1color">8</span>-<span className="h1color">BIT</span></u></h1>
-        <h2><br></br> 1. A robot may not harm a human being or, remaining passive, allow a human being to be exposed to danger.<br></br><br></br>
-        2. A robot must obey the orders given to it by a human being, unless such orders conflict with the First Law.<br></br><br></br>
-        3. A robot must protect its existence so long as such protection does not conflict with the First or Second Law.<br></br><br></br>
-        4. Any robot has the right to reproduce itself, as long as this does not contradict the first, second or third law of robotics.<br></br><br></br></h2>
-        <h3>IA - JWC - HH</h3>
-      </div>
-      <Box 
-          component="img"
-          sx={{
-            maxHeight: { md: 300 },
-            maxWidth: { md: 300 },
-            marginTop: "325px",
-            marginRight: "150px",
-          }}
-          alt="mega"
-          src="/images/mega.gif"
-        />
     </Content>
-    <ImageList sx={{ width: 900, height: 304, marginLeft:'20%' }} cols={6} rowHeight={150}>
+    <ImageList
+      sx={{ width: 900, height: 304, marginLeft: "20%" }}
+      cols={6}
+      rowHeight={150}
+    >
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
           <Image
@@ -122,77 +154,77 @@ const DesktopComponent = ({classes}) =>
       ))}
     </ImageList>
   </Box>
- ;
-
-const Layouts = ({Classes}) => {
-  
+);
+const Layouts = ({ Classes }) => {
   const { width } = useWindowSize();
   const breakpoint = 1200;
-  
-  return width < breakpoint ? <MobileComponent classes={Classes}/> : <DesktopComponent classes={Classes}/>;
+
+  return width < breakpoint ? (
+    <MobileComponent classes={Classes} />
+  ) : (
+    <DesktopComponent classes={Classes} />
+  );
 };
-
-
 
 export default function Eightbit() {
   const classes = useStyles();
   return (
     // <>
-        <Wrapper>
-          <Layouts Classes={classes}/>
-        </Wrapper>
-    // </> 
-  )
+    <Wrapper>
+      <Layouts Classes={classes} />
+    </Wrapper>
+    // </>
+  );
 }
 
 const itemData = [
   {
-    img: '/images/1.png',
-    title: 'Breakfast',
+    img: "/images/1.png",
+    title: "Breakfast",
   },
   {
-    img: '/images/2.png',
-    title: 'Burger',
+    img: "/images/2.png",
+    title: "Burger",
   },
   {
-    img: '/images/3.png',
-    title: 'Camera',
+    img: "/images/3.png",
+    title: "Camera",
   },
   {
-    img: '/images/4.png',
-    title: 'Coffee',
+    img: "/images/4.png",
+    title: "Coffee",
   },
   {
-    img: '/images/5.png',
-    title: 'Hats',
+    img: "/images/5.png",
+    title: "Hats",
   },
   {
-    img: '/images/6.png',
-    title: 'Honey',
+    img: "/images/6.png",
+    title: "Honey",
   },
   {
-    img: '/images/7.png',
-    title: 'Basketball',
+    img: "/images/7.png",
+    title: "Basketball",
   },
   {
-    img: '/images/8.png',
-    title: 'Fern',
+    img: "/images/8.png",
+    title: "Fern",
   },
   {
-    img: '/images/9.png',
-    title: 'Mushrooms',
+    img: "/images/9.png",
+    title: "Mushrooms",
   },
   {
-    img: '/images/10.png',
-    title: 'Tomato basil',
+    img: "/images/10.png",
+    title: "Tomato basil",
   },
   {
-    img: '/images/11.png',
-    title: 'Sea star',
+    img: "/images/11.png",
+    title: "Sea star",
   },
   {
-    img: '/images/12.png',
-    title: 'Bike',
+    img: "/images/12.png",
+    title: "Bike",
   },
 ];
 
@@ -200,9 +232,9 @@ const Wrapper = styled.div`
   height: 100%;
   width: 100%;
   background-image: url("/images/fond.png");
-  background-size:     cover;                      /* <------ */
-  background-repeat:   no-repeat;
-  background-position: center center; 
+  background-size: cover; /* <------ */
+  background-repeat: no-repeat;
+  background-position: center center;
 `;
 const Content = styled.div`
   display: flex;
@@ -212,7 +244,7 @@ const Content = styled.div`
     text-align: left;
     font-family: "NES Controller";
     font-size: clamp(2.65625rem, 0.835rem + 3.795vw, 3.85rem); //last -15%
-    color: 	#02efee;
+    color: #02efee;
     min-width: 0;
   }
   h1 .h1color {
@@ -229,11 +261,11 @@ const Content = styled.div`
     text-decoration: underline;
     text-decoration-color: #02efee;
     text-decoration-thickness: 2px;
-    text-underline-offset: 1rem; 
-}
-.titre {
-  margin-left: 10%; 
-}
+    text-underline-offset: 1rem;
+  }
+  .titre {
+    margin-left: 10%;
+  }
 `;
 const ContentMob = styled.div`
   display: flex;
@@ -257,13 +289,13 @@ const ContentMob = styled.div`
     text-align: justify;
     margin-left: initial;
     margin-right: initial;
-    padding:10%;
-    padding-top:0%;
+    padding: 10%;
+    padding-top: 0%;
   }
   u {
     text-decoration: underline;
     text-decoration-color: #02efee;
     text-decoration-thickness: 2px;
-    text-underline-offset: 1rem; 
-}
+    text-underline-offset: 1rem;
+  }
 `;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,29 @@
 import Eigthbit from "../components/Eigthbit";
 import Scarcity from "../components/Scarcity";
-import { Element } from 'react-scroll'
+import { Element } from "react-scroll";
 import Team from "../components/Team";
 import TheFaq from "../components/TheFaq";
 import Footer from "../components/Footer";
 import DrawAppBar from "../components/DrawAppBar";
 
 const HomePage = () => {
-
-    return (
+  return (
     <>
       <DrawAppBar />
-      <Element id='8-BIT' name='8-BIT'>
-        <Eigthbit/>
+      <Element id="8-BIT" name="8-BIT">
+        <Eigthbit />
       </Element>
-      <Element id='SCARCITY' name='SCARCITY'>
-        <Scarcity/>
+      <Element id="SCARCITY" name="SCARCITY">
+        <Scarcity />
       </Element>
-      <Element id='LAB' name='LAB'>
+      <Element id="LAB" name="LAB">
         <Team />
       </Element>
-      <Element id='FAQ' name='FAQ'>
+      <Element id="FAQ" name="FAQ">
         <TheFaq />
       </Element>
       <Footer />
     </>
-    );
-  };
+  );
+};
 export default HomePage;
